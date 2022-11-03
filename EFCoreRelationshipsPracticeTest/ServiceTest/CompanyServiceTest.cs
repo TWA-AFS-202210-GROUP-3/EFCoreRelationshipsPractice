d9bf1c755a21f17d2441a7747d55d79bc2e6952a
RZhang16 <RZhang16@slb.com>
2022-11-03 18:10:38 +0800

    feat: finish all UT

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -47,12 +47,20 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
         }
 
         [Fact]
-        public async Task Should_create_company_with_profile_success()
+        public async Task Should_delete_company_by_id_success_via_company_service()
         {
             var context = GetCompanyDbContext();
             CompanyDto companyDto = new CompanyDto()
             {
                 Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
                 ProfileDtos = new ProfileDto()
                 {
                     RegisteredCapital = 100010,
@@ -62,9 +70,96 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
 
             CompanyService companyService = new CompanyService(context);
 
-            await companyService.AddCompany(companyDto);
+            var id = await companyService.AddCompany(companyDto);
 
-            Assert.Equal(1, context.Companies.Count());
+            await companyService.DeleteCompany(id);
+
+            Assert.Equal(0, context.Companies.Count());
+        }
+
+        [Fact]
+        public async Task Should_get_company_by_id_success_via_company_service()
+        {
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto()
+            {
+                Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                ProfileDtos = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100",
+                },
+            };
+
+            CompanyService companyService = new CompanyService(context);
+
+            var id = await companyService.AddCompany(companyDto);
+
+            CompanyDto company = await companyService.GetById(id);
+
+            Assert.Equal("IBM", company.Name);
+        }
+
+        [Fact]
+        public async Task Should_get_all_company_by_id_success_via_company_service()
+        {
+            var context = GetCompanyDbContext();
+            List<CompanyDto> companyDtos = new List<CompanyDto>()
+            {
+                new CompanyDto()
+                {
+                    Name = "IBM",
+                    Employees = new List<EmployeeDto>()
+                    {
+                        new EmployeeDto()
+                        {
+                            Name = "Tom",
+                            Age = 19,
+                        },
+                    },
+                    ProfileDtos = new ProfileDto()
+                    {
+                        RegisteredCapital = 100010,
+                        CertId = "100",
+                    },
+                },
+                new CompanyDto()
+                {
+                    Name = "SLB",
+                    Employees = new List<EmployeeDto>()
+                    {
+                        new EmployeeDto()
+                        {
+                            Name = "Tom",
+                            Age = 19,
+                        },
+                    },
+                    ProfileDtos = new ProfileDto()
+                    {
+                        RegisteredCapital = 100010,
+                        CertId = "100",
+                    },
+                },
+            };
+
+            CompanyService companyService = new CompanyService(context);
+
+            foreach (CompanyDto companyDto in companyDtos)
+            {
+                await companyService.AddCompany(companyDto);
+            }
+
+            await companyService.GetAll();
+
+            Assert.Equal(2, context.Companies.Count());
         }
 
         private CompanyDbContext GetCompanyDbContext()
