6d8f1ef958d401ba14be65e6470ebf1ae19a0e8e
Zejie Yao <ZYao3@slb.com>
2022-11-03 17:41:53 +0800

    feat: add Should_create_company_success_via_company_service unit test and pass it

---
+++ EFCoreRelationshipsPracticeTest/ControllerTest/CompanyServiceUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreRelationshipsPractice.Dtos;
+using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCoreRelationshipsPracticeTest.ControllerTest
+{
+    public class CompanyServiceUnitTest : TestBase
+    {
+        public CompanyServiceUnitTest(CustomWebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_create_company_success_via_company_service()
+        {
+            //given
+            var context = getCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto()
+            {
+                Name = "IBM",
+                EmployeesDto = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                ProfileDto = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100",
+                },
+            };
+            CompanyService companyService = new CompanyService(context);
+            //when
+            await companyService.AddCompany(companyDto);
+            //then
+            Assert.Equal(1, context.Companies.Count());
+        }
+
+        private CompanyDbContext getCompanyDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopeService = scope.ServiceProvider;
+            CompanyDbContext context = scopeService.GetRequiredService<CompanyDbContext>();
+            return context;
+        }
+    }
+}
