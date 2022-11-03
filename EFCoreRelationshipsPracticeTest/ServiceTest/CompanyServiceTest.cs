7c87939d22be3be8321fd687a60965fad19d3478
Bingqi <BLiu25@slb.com>
2022-11-03 18:53:18 +0800

    feat: add UT for get all company and get company by id.

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -20,34 +20,14 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
         {
         }
 
-        private CompanyDbContext GetCompanyDbContext()
-        {
-            var scope = Factory.Services.CreateScope();
-            var scopedService = scope.ServiceProvider;
-            CompanyDbContext context = scopedService.GetRequiredService<CompanyDbContext>();
-            return context;
-        }
+
 
         [Fact]
         public async Task Should_create_company_success_via_company_service()
         {
             //given
             var context = GetCompanyDbContext();
-            CompanyDto companyDto = new CompanyDto();
-            companyDto.Name = "IBM";
-            companyDto.Employees = new List<EmployeeDto>()
-            {
-                new EmployeeDto()
-                {
-                    Name = "Tom",
-                    Age = 19,
-                },
-            };
-            companyDto.ProfileDto = new ProfileDto()
-            {
-                RegisteredCapital = 100010,
-                CertId = "100",
-            };
+            var companyDto = CompanyDto();
 
             CompanyService companyService = new CompanyService(context);
 
@@ -89,6 +69,84 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
             Assert.Empty(context.Companies);
         }
 
+        [Fact]
+        public async Task Should_get_all_company_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            var companyDto1 = CompanyDto();
+            CompanyDto companyDto2 = new CompanyDto();
+            companyDto2.Name = "SLB";
+            companyDto2.Employees = new List<EmployeeDto>()
+            {
+                new EmployeeDto()
+                {
+                    Name = "Tom",
+                    Age = 19,
+                },
+            };
+            companyDto2.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
 
+            CompanyService companyService = new CompanyService(context);
+            await companyService.AddCompany(companyDto1);
+            await companyService.AddCompany(companyDto2);
+
+            //when
+            var companyObtained = await companyService.GetAll();
+
+            //then
+            Assert.Equal(2, companyObtained.Count);
+            Assert.Equal("BGC", companyObtained[0].Name);
+            Assert.Equal("SLB", companyObtained[1].Name);
+        }
+
+        [Fact]
+        public async Task Should_get_company_by_Id_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            var companyDto = CompanyDto();
+
+            CompanyService companyService = new CompanyService(context);
+            int companyId = await companyService.AddCompany(companyDto);
+
+            //when
+            var companyObtained = await companyService.GetById(companyId);
+
+            //then
+            Assert.Equal("abc", companyObtained.Name);
+        }
+
+        private static CompanyDto CompanyDto()
+        {
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.Employees = new List<EmployeeDto>()
+            {
+                new EmployeeDto()
+                {
+                    Name = "Tom",
+                    Age = 19,
+                },
+            };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+            return companyDto;
+        }
+
+        private CompanyDbContext GetCompanyDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedService = scope.ServiceProvider;
+            CompanyDbContext context = scopedService.GetRequiredService<CompanyDbContext>();
+            return context;
+        }
     }
 }
