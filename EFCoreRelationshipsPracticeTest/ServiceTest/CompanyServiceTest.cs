800de9bfba910450ed6f8b6c27ddb7ac5706198a
Fengjun Ma <FMa6@slb.com>
2022-11-03 17:55:57 +0800

    test: add unit of create company

---
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using EFCoreRelationshipsPractice.Dtos;
+using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreRelationshipsPracticeTest.ServiceTest
+{
+    public class CompanyServiceTest : TestBase
+    {
+        public CompanyServiceTest(CustomWebApplicationFactory<Program> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_Create_company_successfully()
+        {
+            //given
+            var context = GetCompanyDbContext();
+
+            CompanyDto companyDto = new CompanyDto
+            {
+                Name = "IBM",
+                EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                ProfileDto = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100",
+                },
+            };
+            var companyService = new CompanyService(context);
+
+            //when
+            await companyService.AddCompany(companyDto);
+
+            //then
+            Assert.Equal(1, context.Companies.Count());
+        }
+
+        public CompanyDbContext GetCompanyDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedService = scope.ServiceProvider;
+            CompanyDbContext context = scopedService.GetService<CompanyDbContext>();
+            return context;
+        }
+    }
+}
